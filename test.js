5102016fe187c6015340f9b6b8c3f1ceab93f014
NicolenaStiles <nicolena.stiles@gmail.com>
2023-02-20 01:48:59 -0500

    Can now make the menu disappear... now I need something neat to show back there.

---
+++ test.js
@@ -0,0 +1,5 @@
+// changes the background display and makes the main menu disappear
+function bg_button() {
+    document.body.style.backgroundImage = "url('images/oceans_brawl.jpeg')";
+    document.getElementById("ctr").setAttribute("class","d-none");
+}
