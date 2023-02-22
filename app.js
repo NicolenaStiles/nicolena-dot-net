803d7c9a0647a263e3b6a79e760aa51bc87af205
NicolenaStiles <nicolena.stiles@gmail.com>
2023-02-22 04:44:40 -0500

    cleanup needed but canvas now properly resizes. thought I had this correct last time but it was not actually done the right way (oops)

--- app.js
+++ app.js
@@ -6,6 +6,92 @@
 // triggers on load of body
 function on_load () {
 
+    const canvas = document.getElementById('myCanvas');
+    const ctx = canvas.getContext('2d');
+  
+    // resize the canvas to fill browser window dynamically
+    window.addEventListener('resize', resizeCanvas, false);
+
+    function drawStuff() {
+    
+        // set initial conditions
+        // center of canvas
+        var cx = canvas.getAttribute("width")/2;
+        var cy = canvas.getAttribute("height")/2;
+
+        // spaceship dimensions
+        var w = 35;
+        var h = 65;
+        var linewidth = 3;
+
+        // drawing settings
+        ctx.strokeStyle = "white"
+        ctx.lineWidth = linewidth;
+
+            // starting position
+        // movement speed/direction
+        var x = cx;
+        var y = cy;
+        var dx = 0;
+        var dy = -2;
+
+        // define points:
+        var a = {
+            "x" : cx, 
+            "y" : cy - (0.5 * h)
+        }
+        
+        var b = {
+            "x" : cx - (0.35 * w), 
+            "y" : cy + (0.25 * h)
+        }
+
+        var c = {
+            "x" : cx - (0.5 * w), 
+            "y" : cy + (0.5 * h)
+        }
+
+        var d = {
+            "x" : cx + (0.35 * w), 
+            "y" : cy + (0.25 * h)
+        }
+
+        var e = {
+            "x" : cx + (0.5 * w), 
+            "y" : cy + (0.5 * h)
+        }
+
+        // do your drawing stuff here
+        ctx.beginPath();
+        ctx.moveTo(a.x + dx, a.y + dy);
+        ctx.lineTo(c.x + dx, c.y + dy);
+        ctx.stroke();
+        ctx.moveTo(b.x + dx, b.y + dy);
+        ctx.lineTo(d.x + dx, d.y + dy);
+        ctx.stroke()
+        ctx.moveTo(e.x + dx, e.y + dy);
+        ctx.lineTo(a.x + dx, a.y + dy);
+        ctx.stroke();
+        ctx.closePath();
+    }
+          
+    function resizeCanvas() {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+                  
+      /**
+       * Your drawings need to be inside this function otherwise they will be reset when 
+       * you resize the browser window and the canvas goes will be cleared.
+       */
+      drawStuff(); 
+    }
+    
+    resizeCanvas();
+
+
+    /*
+
+
     // resizes canvas to be the size of the screen
     //document.getElementById('myCanvas').setAttribute("width", document.body.clientWidth);
     //document.getElementById('myCanvas').setAttribute("height", document.body.clientHeight);
@@ -112,4 +198,5 @@ function on_load () {
     }
 
     setInterval(draw, 10);
+    */
 }
