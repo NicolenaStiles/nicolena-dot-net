b66412109ca455629697b8b59821ecaeae0658d9
NicolenaStiles <nicolena.stiles@gmail.com>
2023-03-04 03:45:10 -0500

    pausing all this to switch to a dev folder and mess with relevant refactor stuff

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ const ctx = canvas.getContext('2d');
 // resize the canvas to fill browser window dynamically
 window.addEventListener('resize', resizeCanvas, false);
 
+console.log("hello from index.js outside of function!");
+
 function resizeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
                 
     // main app entry point
+    console.log("hello from index.js in resizecanvas!");
     run_asteroids(canvas, ctx);
 }
 
